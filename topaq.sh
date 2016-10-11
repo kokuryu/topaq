e377c20e2c76a9a05b467d0e8dbae0283d003333
kokuryu <kokuryu@e-mail.ph>
2016-10-11 11:45:32 +0200

    Create topaq.sh

---
+++ topaq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# 2paq- Explorador de paquetes
+# Herramienta para listar paquetes del sistema.
+#VARIABLES
+correcto="1"
+#PANTALLA PRINCIPAL-ASIGNAR NOMBRE DE ARCHIVO
+	echo "Leyendo paquetes"
+	echo "Asigna un nombre al archivo donde guardar la lista de paquetes"
+#NOMBRE DE ARCHIVO
+	read nombre
+#CREAR ARCHIVO	
+dpkg -l > $nombre.txt
+#SEGUNDA PANTALLA-LEER ARCHIVO
+	echo "Archivo de texto $nombre creado"
+	
+	echo "Quieres leerlo?"
+	echo "1.Si"
+	echo "2.No"
+#RESPUESTA A LEER ARCHIVO
+	read leer
+#ABRIR ARCHIVO
+if leer=$correcto
+then gedit $nombre.txt;
+else
+exit
+fi
